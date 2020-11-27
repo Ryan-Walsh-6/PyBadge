8f75cc9516b23da536c8f644f478b359dfc94a39
Ryan-Walsh-6 <74618988+Ryan-Walsh-6@users.noreply.github.com>
2020-11-27 13:35:07 -0500

    Create code.py

---
+++ Lesson1/code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Ryan Walsh
+# Created on: November 2020
+# This program is the "Space Aliens" program on the PyBadge
+
+def main():
+    # this function is the main game game_scene
+
+    print("\n\n\n") # 3 blank Lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    main()
